1592e7090502c0661a26fb281ceefad1f6cdbdf3
Eleven <lhqFH2818>
2017-12-21 21:40:22 +0800

    test

--- python/main.py
+++ python/main.py
@@ -71,5 +71,5 @@ class okex():
 if __name__ == '__main__':
     api = okex()
     while(1):
-        api.good_trade(['btc', 'eth', 'mco'], Threshold=1.04)
+        api.good_trade(['btc', 'eth', 'mco'], Threshold=1.02)
         sleep(1)
